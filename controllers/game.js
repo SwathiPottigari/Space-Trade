4c6810f5d2416fcc2b29c8854f7218c5b4f99ee2
Okyfunoky <okyfunoky@hotmail.com>
2019-10-11 12:57:16 -0700

    Files linked for routing

---
+++ controllers/game.js
@@ -0,0 +1,36 @@
+let game = {
+    loadGame : function(){
+
+    },
+    saveGame : function(){
+        console.log("Files associated")
+    },
+    displayPlanetaryGoods : function(){
+
+    },
+    processTrade : function (){
+    //probably need an object that holds all the values for the various trade values
+    //buying - { products {product1, amount},{product2, amount}}
+    //selling { products {product1, amount},{product2, amount}}
+    //get total value to add/subtract for money
+    },
+    updatePlanetaryStock : function(){
+
+    },
+    updatePlayerMoney : function(){
+
+    },
+    updatePlayerStock : function(){
+
+    },
+    updatePlayerFuel : function(){
+
+    },
+    updatePlanetaryHappiness : function(){
+
+    },
+    updateGalacticHappiness : function(){
+
+    }
+}
+module.exports = game;
