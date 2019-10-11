cd260341c2b6c18f930e05948736de6c8af21cf9
J-Anne Carlson <carlsonjc3@J-Annes-MacBook-Pro.local>
2019-10-11 13:48:09 -0700

    added resources

--- models/planet.js
+++ models/planet.js
@@ -1,3 +1,4 @@
+// **SEQUELIZE PLANET TABLE** //
 module.exports = function(sequelize, DataTypes) {
     var Planet = sequelize.define("Planet", {
       planetName: {
@@ -7,32 +8,28 @@ module.exports = function(sequelize, DataTypes) {
           len: [1]
         }
       },
+      
       planetUnique: {
         type: DataTypes.TEXT,
-       
       },
+
       planetFavorite: {
         type: DataTypes.TEXT,
         allowNull: false,
-
       },
 
       planetImage: {
         type: DataTypes.TEXT,
         allowNull: false,
-
       },
 
       planetTrader: {
         type: DataTypes.TEXT,
         allowNull: false,
-
       },
 
       planetStory: {
         type: DataTypes.TEXT,
-        
-
       },
     });
     return Planet;
