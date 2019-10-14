cf0c430df268fe84ebd63a08a9fea13d0ede34de
Okyfunoky <okyfunoky@hotmail.com>
2019-10-14 12:59:23 -0700

    Starter code for login buttons

---
+++ public/assets/js/login.js
@@ -0,0 +1,17 @@
+$('#loginBtn').click(function(event){
+    console.log("Login!")
+    event.preventDefault();
+    var email = $('#email').val();
+    var pword = $('#pword').val();
+    console.log("User inputs...." + email + pword);
+    //"get" the start game page
+})
+
+$('#newAccountBtn').click(function(event){
+    console.log("New Account!")
+    event.preventDefault();
+    var email = $('#email').val();
+    var pword = $('#pword').val();
+
+    //get the start page
+})
