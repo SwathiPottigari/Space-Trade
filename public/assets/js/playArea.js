a9013bbb821e828dc91ce46484cddadb4f0c5475
Willie Edwards <gammaray360@gmail.com>
2019-10-13 19:38:19 -0700

    wrote functions to style planets

--- public/assets/js/playArea.js
+++ public/assets/js/playArea.js
@@ -1,56 +1,99 @@
-// Create the general play area using a canvas
-
-// $(document).ready(function() {
-//     startGame();
-// });
-
-// function startGame() {
-//     playArea.start();
-// }
-
-// var playArea = {
-//     canvas : document.createElement("canvas"),
-//     start : function() {
-//         this.canvas.width = 960;
-//         this.canvas.height = 540;
-//         this.context = this.canvas.getContext("2d");
-//         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-//     }
-// }
-
 // Create five planets to be displayed in the play area
 
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.beginPath();
-ctx.arc(800, 90, 45, 0, 2 * Math.PI);
-ctx.stroke();
-ctx.fill();
+function drawPlanet1() {
 
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.beginPath();
-ctx.arc(100, 440, 50, 0, 2 * Math.PI);
-ctx.stroke();
-ctx.fill();
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
 
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.beginPath();
-ctx.arc(150, 100, 60, 0, 2 * Math.PI);
-ctx.stroke();
-ctx.fill();
+    // Create new image object to use as pattern
+    var img = new Image();
+    img.src = 'https://art.pixilart.com/d54aecfdf01cf9c.png';
+    img.onload = function () {
 
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.beginPath();
-ctx.arc(460, 260, 70, 0, 2 * Math.PI);
-ctx.stroke();
-ctx.fill();
+        var ptrn = ctx.createPattern(img, 'repeat');
+        // Planet 1
+        ctx.beginPath();
+        ctx.fillStyle = ptrn;
+        ctx.arc(800, 90, 45, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.closePath();
+    };
+};
 
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.beginPath();
-ctx.arc(850, 450, 75, 0, 2 * Math.PI);
-ctx.stroke();
-ctx.fill();
+function drawPlanet2() {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    // Create new image object to use as pattern
+    var img = new Image();
+    img.src = 'https://art.pixilart.com/8ced160f7c09a76.png';
+    img.onload = function () {
+
+        var ptrn = ctx.createPattern(img, 'repeat');
+        // Planet 2
+        ctx.beginPath();
+        ctx.fillStyle = ptrn;
+        ctx.arc(100, 440, 50, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.closePath();
+    };
+};
+
+function drawPlanet3() {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    // Create new image object to use as pattern
+    var img = new Image();
+    img.src = 'https://art.pixilart.com/7c43c6fb94e4d50.gif';
+    img.onload = function () {
+
+        var ptrn = ctx.createPattern(img, 'repeat');
+        // Planet 3
+        ctx.beginPath();
+        ctx.fillStyle = ptrn;
+        ctx.arc(150, 100, 60, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.closePath();
+    };
+};
+
+function drawPlanet4() {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    // Create new image object to use as pattern
+    var img = new Image();
+    img.src = 'https://art.pixilart.com/thumb/248848f0a2e9b95.png';
+    img.onload = function () {
+
+        var ptrn = ctx.createPattern(img, 'repeat');
+        // Planet 4
+        ctx.beginPath();
+        ctx.fillStyle = ptrn;
+        ctx.arc(460, 260, 70, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.closePath();
+    };
+};
+
+function drawPlanet5() {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    // Create new image object to use as pattern
+    var img = new Image();
+    img.src = 'https://art.pixilart.com/thumb/0ad802cc8e094bc.png';
+    img.onload = function () {
+
+        var ptrn = ctx.createPattern(img, 'repeat');
+        // Planet 5
+        ctx.beginPath();
+        ctx.fillStyle = ptrn;
+        ctx.arc(850, 450, 75, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.closePath();
+    };
+};
+
+drawPlanet1();
+drawPlanet2();
+drawPlanet3();
+drawPlanet4();
+drawPlanet5();
