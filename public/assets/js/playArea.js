0fb3aada2592d854f4e5962b97a55566437befc0
J-Anne Carlson <carlsonjc3@J-Annes-MacBook-Pro.local>
2019-10-15 09:25:01 -0700

    planet test table added

--- public/assets/js/playArea.js
+++ public/assets/js/playArea.js
@@ -1,4 +1,3 @@
-
 // import { finished } from "stream";
 
 // Create five planets to be displayed in the play area
@@ -12,74 +11,165 @@
 //         isWon: false,
 //         planets: [
 //             {
-//                 id: 1,
-//                 resources: [{
-//                     id:20,
-//                     resName:"fish",
-//                     resCount: 10
-//                 }, {
-//                     id:30,
-//                     resName:"fish",
-//                     resCount: 20
-//                 }
+//              id: planetONE
+//              // **Cat-a-stropohic Chaos** //
+//              name: Cat-a-stropohic Chaos ,
+//              resources: [{
+//                     resID: 1,
+//                     resName: Food,
+//                     resCount: 5,
+
+//                     resID: 2,
+//                     resName: Medicine,
+//                     resCount: 5,
+
+//                     resID: 3,
+//                     resName: Entertainment,
+//                     resCount: 5,
+
+//                     resID: 4,
+//                     resName: Technology,
+//                     resCount: 5,
+
+//                     resID: 5,
+//                     resName: Water,
+//                     resCount: 5,
+//                     }, 
 //                 ],
 //                 happinessCount: 20,
 //                 isHappy: true
-//             },
-//             {
-//                 id: 4,
-//                 resources: [{
-//                     id:20,
-//                     resName:"choc",
-//                     resCount: 30
 //                 },
+
 //                 {
-//                     id:20,
-//                     resName:"choc",
-//                     resCount: 40
-//                 }
+//                 id: planetTWO,
+//                 // **Deja-Moo** //
+//              id: planetTWO
+//              name: Deja-Moo,
+//              resources: [{
+//                     resID: 1,
+//                     resName: Food,
+//                     resCount: 5,
+
+//                     resID: 2,
+//                     resName: Medicine,
+//                     resCount: 5,
+
+//                     resID: 3,
+//                     resName: Entertainment,
+//                     resCount: 5,
+
+//                     resID: 4,
+//                     resName: Technology,
+//                     resCount: 5,
+
+//                     resID: 5,
+//                     resName: Water,
+//                     resCount: 5,
+//                     }, 
 //                 ],
-//                 happinessCount: 30,
-//                 isHappy: false,
-//             },
-//             {
-//                 id: 2,
-//                 resources: [{
-//                     id:20,
-//                     resName:"water",
-//                     resCount: 50
+//                 happinessCount: 20,
+//                 isHappy: true
 //                 },
-//                 {
-//                     id:20,
-//                     resName:"water",
-//                     resCount: 60
-//                 }
+//
+//                      {
+//                  id: planetTHREE
+//                  // **Hue-Manatee** //
+//                  name: Hue-Manatee ,
+//                  resources: [{
+//                     resID: 1,
+//                     resName: Food,
+//                     resCount: 5,
+    
+//                     resID: 2,
+//                     resName: Medicine,
+//                     resCount: 5,
+    
+//                     resID: 3,
+//                     resName: Entertainment,
+//                     resCount: 5,
+    
+//                     resID: 4,
+//                     resName: Technology,
+//                     resCount: 5,
+    
+//                     resID: 5,
+//                     resName: Water,
+//                     resCount: 5,
+//                     }, 
 //                 ],
-//                 happinessCount: 30,
-//                 isHappy: true,
-//             },
-//             {
-//                 id: 3,
-//                 resources: [{
-//                     id:20,
-//                     resName:"ice",
-//                     resCount: 70
+//                 happinessCount: 20,
+//                 isHappy: true
 //                 },
-//                 {
-//                     id:20,
-//                     resName:"ice",
-//                     resCount: 80
-//                 }
-//                 ],
-//                 happinessCount: 80,
-//                 isHappy: true,
-//             }
-//         ],
+
+//                      {
+//                  id: planetFOUR
+//                  // **Dumble-dope** //
+//                  name: Dumble-dope ,
+//                  resources: [{
+//                     resID: 1,
+//                     resName: Food,
+//                     resCount: 5,
         
+//                     resID: 2,
+//                     resName: Medicine,
+//                     resCount: 5,
+        
+//                     resID: 3,
+//                     resName: Entertainment,
+//                     resCount: 5,
+        
+//                     resID: 4,
+//                     resName: Technology,
+//                     resCount: 5,
+        
+//                     resID: 5,
+//                     resName: Water,
+//                     resCount: 5,
+//                     }, 
+//                 ],
+//                 happinessCount: 20,
+//                 isHappy: true
+//                 },
+
+//                      {
+//                  id: planetFIVE
+//                  // **Ice-olated** //
+//                  name: Ice-olated ,
+//                  resources: [{
+//                     resID: 1,
+//                     resName: Food,
+//                     resCount: 5,
+        
+//                     resID: 2,
+//                     resName: Medicine,
+//                     resCount: 5,
+        
+//                     resID: 3,
+//                     resName: Entertainment,
+//                     resCount: 5,
+        
+//                     resID: 4,
+//                     resName: Technology,
+//                     resCount: 5,
+        
+//                     resID: 5,
+//                     resName: Water,
+//                     resCount: 5,
+//                     }, 
+//                 ],
+//                 happinessCount: 20,
+//                 isHappy: true
+//                 },
+
+
+    
+
+
+// ==========================================================================
 //     };
 //     saveGame(data);
 
-    // To test to get  the data by Id
+// To test to get  the data by Id
 //    var data="Test";
 //     getById(data);
 // });
@@ -113,28 +203,28 @@ $(document).ready(function () {
 
 // Create space ship game piece
 
-function drawShip() { 
+function drawShip() {
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
     // Draw saucer bottom. 
-    ctx.beginPath(); 
-    ctx.moveTo(28.4, 16.9); 
-    ctx.bezierCurveTo(28.4, 19.7, 22.9, 22.0, 16.0, 22.0); 
-    ctx.bezierCurveTo(9.1, 22.0, 3.6, 19.7, 3.6, 16.9); 
-    ctx.bezierCurveTo(3.6, 14.1, 9.1, 11.8, 16.0, 11.8); 
-    ctx.bezierCurveTo(22.9, 11.8, 28.4, 14.1, 28.4, 16.9); 
-    ctx.closePath(); 
-    ctx.fillStyle = "salmon"; 
-    ctx.fill(); 
+    ctx.beginPath();
+    ctx.moveTo(28.4, 16.9);
+    ctx.bezierCurveTo(28.4, 19.7, 22.9, 22.0, 16.0, 22.0);
+    ctx.bezierCurveTo(9.1, 22.0, 3.6, 19.7, 3.6, 16.9);
+    ctx.bezierCurveTo(3.6, 14.1, 9.1, 11.8, 16.0, 11.8);
+    ctx.bezierCurveTo(22.9, 11.8, 28.4, 14.1, 28.4, 16.9);
+    ctx.closePath();
+    ctx.fillStyle = "salmon";
+    ctx.fill();
     // Draw saucer top. 
-    ctx.beginPath(); 
-    ctx.moveTo(22.3, 12.0); 
-    ctx.bezierCurveTo(22.3, 13.3, 19.4, 14.3, 15.9, 14.3); 
-    ctx.bezierCurveTo(12.4, 14.3, 9.6, 13.3, 9.6, 12.0); 
-    ctx.bezierCurveTo(9.6, 10.8, 12.4, 9.7, 15.9, 9.7); 
-    ctx.bezierCurveTo(19.4, 9.7, 22.3, 10.8, 22.3, 12.0); 
-    ctx.closePath(); 
-    ctx.fillStyle = "aquamarine"; 
+    ctx.beginPath();
+    ctx.moveTo(22.3, 12.0);
+    ctx.bezierCurveTo(22.3, 13.3, 19.4, 14.3, 15.9, 14.3);
+    ctx.bezierCurveTo(12.4, 14.3, 9.6, 13.3, 9.6, 12.0);
+    ctx.bezierCurveTo(9.6, 10.8, 12.4, 9.7, 15.9, 9.7);
+    ctx.bezierCurveTo(19.4, 9.7, 22.3, 10.8, 22.3, 12.0);
+    ctx.closePath();
+    ctx.fillStyle = "aquamarine";
     ctx.fill();
 };
 
