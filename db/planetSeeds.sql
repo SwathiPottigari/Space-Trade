0b3e00a68352518658c2bd2719ec85fcaf8a4537
Willie Edwards <gammaray360@gmail.com>
2019-10-16 12:12:49 -0700

    reformatted style.css

--- db/planetSeeds.sql
+++ db/planetSeeds.sql
@@ -1,15 +1,17 @@
+INSERT INTO planets(planetName,planetUnique,planetFavorite,planetImage,planetTrader,planetStory,createdAt,updatedAt)
+VALUES ("Osiris","x","x","/assets/images/space-cat1.png","FLAVOR TEXT HERE","x",NOW(),now());
 
 INSERT INTO planets(planetName,planetUnique,planetFavorite,planetImage,planetTrader,planetStory,createdAt,updatedAt)
-VALUES("Cat-a-trophic Chaos","Space Catnip","Space Fertilizer","IMAGE HERE","IMAGE HERE","CONNECT PLANET STORY",NOW(),now());
+VALUES ("Anubis","x","x","/assets/images/space-cat2.png","FLAVOR TEXT HERE","x",NOW(),now());
 
 INSERT INTO planets(planetName,planetUnique,planetFavorite,planetImage,planetTrader,planetStory,createdAt,updatedAt)
-VALUES("Deja Moo","Space Fertilizer","Space Technology","IMAGE HERE","IMAGE HERE","CONNECT PLANET STORY",NOW(),now());
+VALUES ("Bastion","x","x","/assets/images/space-cat3.png","FLAVOR TEXT HERE","x",NOW(),now());
 
 INSERT INTO planets(planetName,planetUnique,planetFavorite,planetImage,planetTrader,planetStory,createdAt,updatedAt)
-VALUES ("Hue-Manatee","Space Technology","Space Catnip","IMAGE HERE","IMAGE HERE","CONNECT PLANET STORY",NOW(),now());
+VALUES ("Nephthys","x","x","/assets/images/space-cat5.png","FLAVOR TEXT HERE","x",NOW(),now());
 
 INSERT INTO planets(planetName,planetUnique,planetFavorite,planetImage,planetTrader,planetStory,createdAt,updatedAt)
-VALUES ("Dumble-dope","Space Crystals","Space Snowcones","IMAGE HERE","IMAGE HERE","CONNECT PLANET STORY",NOW(),now());
+VALUES ("Sobek","x","x","/assets/images/space-cat4.png","FLAVOR TEXT HERE","x",NOW(),now());
 
 INSERT INTO planets(planetName,planetUnique,planetFavorite,planetImage,planetTrader,planetStory,createdAt,updatedAt)
-VALUES ("Ice-olated","Space Snowcones","Space Crystals","IMAGE HERE","IMAGE HERE","CONNECT PLANET STORY",NOW(),now());
+VALUES ("USER","x","x","/assets/images/space-cat1.png","FLAVOR TEXT HERE","x",NOW(),now());
